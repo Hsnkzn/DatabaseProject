126df5680439041c6db6df9a2ebfac8ff9027b30
Hassan Bazzi <hassanbazzi279@gmail.com>
2025-06-15 09:27:42 +0300

    hasan

--- src/Office.java
+++ src/Office.java
@@ -1,68 +1,101 @@
 import javax.persistence.*;
-import java.util.*;
+import java.util.List;
 
 @Entity
-@Table(name = "Offices")
 public class Office {
     @Id
-    @Column(name = "officeCode")
     private String officeCode;
-
-    @Column(name = "city")
     private String city;
-
-    @Column(name = "phone")
     private String phone;
-
-    @Column(name = "addressLine1")
     private String addressLine1;
-
-    @Column(name = "addressLine2")
     private String addressLine2;
-
-    @Column(name = "state")
     private String state;
-
-    @Column(name = "country")
     private String country;
-
-    @Column(name = "postalCode")
     private String postalCode;
-
-    @Column(name = "territory")
     private String territory;
 
     @OneToMany(mappedBy = "office")
-    private List<Employee> employees = new ArrayList<>();
+    private List<Employee> employees;
 
-    // Getters and Setters
-    public String getOfficeCode() { return officeCode; }
-    public void setOfficeCode(String officeCode) { this.officeCode = officeCode; }
+    // Getters and setters
 
-    public String getCity() { return city; }
-    public void setCity(String city) { this.city = city; }
+    public String getOfficeCode() {
+        return officeCode;
+    }
 
-    public String getPhone() { return phone; }
-    public void setPhone(String phone) { this.phone = phone; }
+    public void setOfficeCode(String officeCode) {
+        this.officeCode = officeCode;
+    }
 
-    public String getAddressLine1() { return addressLine1; }
-    public void setAddressLine1(String addressLine1) { this.addressLine1 = addressLine1; }
+    public String getCity() {
+        return city;
+    }
 
-    public String getAddressLine2() { return addressLine2; }
-    public void setAddressLine2(String addressLine2) { this.addressLine2 = addressLine2; }
+    public void setCity(String city) {
+        this.city = city;
+    }
 
-    public String getState() { return state; }
-    public void setState(String state) { this.state = state; }
+    public String getPhone() {
+        return phone;
+    }
 
-    public String getCountry() { return country; }
-    public void setCountry(String country) { this.country = country; }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 
-    public String getPostalCode() { return postalCode; }
-    public void setPostalCode(String postalCode) { this.postalCode = postalCode; }
+    public String getAddressLine1() {
+        return addressLine1;
+    }
 
-    public String getTerritory() { return territory; }
-    public void setTerritory(String territory) { this.territory = territory; }
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
 
-    public List<Employee> getEmployees() { return employees; }
-    public void setEmployees(List<Employee> employees) { this.employees = employees; }
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getTerritory() {
+        return territory;
+    }
+
+    public void setTerritory(String territory) {
+        this.territory = territory;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
 }
